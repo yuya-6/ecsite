9eca0bb508d78ac451a867a65a64f549e14c7036
yuya-6 <yuchongakufu@gmail.com>
2024-08-22 21:30:46 +0900

    login.jspの作成まで

---
+++ src/com/diworksdev/ecsite/action/HomeAction.java
@@ -0,0 +1,31 @@
+package com.diworksdev.ecsite.action;
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.Map;
+import org.apache.struts2.interceptor.SessionAware;
+import com.diworksdev.ecsite.dao.BuyItemDAO;
+import com.diworksdev.ecsite.dto.BuyItemDTO;
+
+public class HomeAction extends ActionSupport implements SessionAware {
+	public Map<String, Object> session;
+	public String execute() {
+		String result = "login";
+		if (session.containsKey("login_user_id")) {
+			BuyItemDAO buyItemDAO = new BuyItemDAO ();
+			BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
+
+			session.put("id", buyItemDTO.getId());
+			session.put ("buyItem_name", buyItemDTO.getItemName());
+			session.put("buyItem_price", buyItemDTO.getItemPrice());
+			result = SUCCESS;
+		}
+		return result;
+		@Override
+		public void setSession(Map<String,Object>session){
+			this.session = session;
+		}
+
+		public Map<String, Object> getSession() {
+			return this.session;
+		}
+	}
+}
